fcf3e4086be678f004ab256678df277e7d26593a
hydeandjek <hydeandjek@naver.com>
2023-11-22 09:48:29 +0900

    #7. 종합 연습 예제1

--- src/main/java/com/study/jpa/chap05_practice/repository/HashTagRepository.java
+++ src/main/java/com/study/jpa/chap05_practice/repository/HashTagRepository.java
@@ -4,4 +4,7 @@ import com.study.jpa.chap05_practice.entity.HashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HashTagRepository extends JpaRepository<HashTag,Long> {
+
+
+
 }
